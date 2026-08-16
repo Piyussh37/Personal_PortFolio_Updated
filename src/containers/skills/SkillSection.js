53aa5187cd97db908f94b9c57978c00860d89cfc
tusharw-mplussoft <tusharwavare@mplussoft.in>
2026-08-16 12:54:12 +0530

    changes jayesh

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -3,9 +3,9 @@ import "./Skills.css";
 import SoftwareSkill from "../../components/softwareSkills/SoftwareSkill";
 import { skills } from "../../portfolio";
 import { Fade } from "react-reveal";
-import DataScienceImg from "./DataScienceImg";
-import FullStackImg from "./FullStackImg";
-import CloudInfraImg from "./CloudInfraImg";
+// import DataScienceImg from "./DataScienceImg";
+// import FullStackImg from "./FullStackImg";
+// import CloudInfraImg from "./CloudInfraImg";
 import DesignImg from "./DesignImg";
 
 function GetSkillSvg(props) {
